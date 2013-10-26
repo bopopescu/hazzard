6f651e5289d5388018b08a63f05cd8abbd78cbc7
Prisa <air_pd2@hotmail.co.th>
2013-10-26 22:28:08 +0700

    update UI
    cusotmer
    officer
    functionprocess.js

--- app/static/js/process_function.js
+++ app/static/js/process_function.js
@@ -1,149 +1,67 @@
-function register_request_listFunction(){
 
-	// var el = document.getElementById('register_request_list');
-	// el.style.display = 'block';
-
-	// document.getElementById('intro_officer').style.display = 'none';
-
-	document.getElementById('register_request_list').style.display = 'block';
+//customer
+function hideCustomerFunction(){
 	document.getElementById('intro_officer').style.display = 'none';
+	document.getElementById('register_request_list').style.display = 'none';
 	document.getElementById('sample_request_list').style.display = 'none';
 
-	// var ch = document.getElementById('modify_link');
-	// ch.setAttribute("href", "modify_register_request_customer.html");
+}
 
-
-	// var mn = document.getElementById('extend_link');
-	// mn.setAttribute("href","extend_register_request_customer.html");
+function register_request_listFunction(){
+	hideCustomerFunction();
+	document.getElementById('register_request_list').style.display = 'block';
 
 }
 
 
 function sample_request_listFunction(){
-
-
-	document.getElementById('register_request_list').style.display = 'none';
-	document.getElementById('intro_officer').style.display = 'none';
+	hideCustomerFunction();
 	document.getElementById('sample_request_list').style.display = 'block';
-
-}
-// function modify_register_request_listFunction(){
-
-// 	var el = document.getElementById('modify_register_request_list');
-// 	el.style.visibility = 'visible';
-
-// 	var ch = document.getElementById('modify_link');
-// 	ch.setAttribute("href", "modify_register_request_customer.html");
-
-
-// 	var mn = document.getElementById('extend_link');
-// 	mn.setAttribute("href","extend_register_request_customer.html");
-
-// }
-
-// function extend_register_request_listFunction(){
-
-// 	var el = document.getElementById('extend_register_request_list');
-// 	el.style.visibility = 'visible';
-
-// 	// var ch = document.getElementById('modify_link');
-// 	// ch.setAttribute("href", "modify_register_request_customer.html");
-
-
-// 	var mn = document.getElementById('extend_link');
-// 	mn.setAttribute("href","extend_register_request_customer.html");
-
-// }
-
-function substitute_register_request_listFunction(){
-
-	var el = document.getElementById('substitute_register_request_list');
-	el.style.display = '';
-
-	// var ch = document.getElementById('modify_link');
-	// ch.setAttribute("href", "modify_register_request_customer.html");
-
-
-	// var mn = document.getElementById('extend_link');
-	// mn.setAttribute("href","extend_register_request_customer.html");
 
 }
 
 //officer
-function registerFunction(){
-
-
+function hideOfficerFunction(){
 
 	document.getElementById('intro_officer').style.display = 'none';
+	document.getElementById('registerProcess_list').style.display = 'none';
+	document.getElementById('modify_registerProcess_list').style.display = 'none';
+	document.getElementById('extend_registerProcess_list').style.display = 'none';
+	document.getElementById('substitue_registerProcess_list').style.display = 'none';
+	document.getElementById('substitue_registerProcess_list').style.display = 'none';
+	document.getElementById('sampleProduceProcess_list').style.display = 'none';
+	document.getElementById('sampleImportProcess_list').style.display = 'none';
+}
+
+function registerFunction(){
 	document.getElementById('registerProcess_list').style.display = 'block';
-	 document.getElementById('modify_registerProcess_list').style.display = 'none';
-	 document.getElementById('extend_registerProcess_list').style.display = 'none';
-	 document.getElementById('substitue_registerProcess_list').style.display = 'none';
-
-
-
-
-	// var list_Title = document.getElementById("list_Title");
-	// list_Title.innerHTML='รายชื่อผู้ชื่อ ผู้ขอใบขึ้นทะเบียนวัตถุอันตราย';
-
-	// var ch = document.getElementById('check_value_link');
-	// ch.setAttribute("href", "register_request_view_officer.html");
-
-
-	// var mn = document.getElementById('manage_value_link');
-	// mn.setAttribute("href","register_permit_officer.html");
-
-
 }
 
 function modify_registerFunction(){
-
-	document.getElementById('intro_officer').style.display = 'none';
-	 document.getElementById('modify_registerProcess_list').style.display = 'block';
-	 document.getElementById('registerProcess_list').style.display = 'none';
-	 document.getElementById('extend_registerProcess_list').style.display = 'none';
-	 document.getElementById('substitue_registerProcess_list').style.display = 'none';
-
-
-
-	// var list_Title = document.getElementById("list_Title");
-	// list_Title.innerHTML='รายชื่อผู้ประการที่ขออนญาตเปลี่ยนแปลงข้อมูลใน ใบอนุญาตขึ้นทะเบียนวัตถุอันตราย';
-
-	// var ch = document.getElementById('check_value_link');
-	// ch.setAttribute("href", "modify_register_request_customer.html");
-
-	// var mn = document.getElementById('manage_value_link');
-	// mn.setAttribute("href","modify_register_permit_officer.html");
-
-    
+	hideOfficerFunction();
+	document.getElementById('modify_registerProcess_list').style.display = 'block';
 }
 
 function extend_registerFunction(){
-	document.getElementById('intro_officer').style.display = 'none';
-	 document.getElementById('modify_registerProcess_list').style.display = 'none';
-	 document.getElementById('registerProcess_list').style.display = 'none';
+	 hideOfficerFunction();
 	  document.getElementById('extend_registerProcess_list').style.display = 'block';
-	   document.getElementById('substitue_registerProcess_list').style.display = 'none';
-
-
-
-
-	// var list_Title = document.getElementById("list_Title");
-	// list_Title.innerHTML='รายชื่อผู้ประการที่ขออนญาตต่ออายุ ใบอนุญาตขึ้นทะเบียนวัตถุอันตราย'
-
- //    var ch = document.getElementById('check_value_link');
-	// ch.setAttribute("href", "extend_register_request_customer.html");
-
-	// var mn = document.getElementById('manage_value_link');
-	// mn.setAttribute("href","extend_register_permit_officer.html");
-    
 }
 
 
 function substitue_registerFunction(){
-	document.getElementById('intro_officer').style.display = 'none';
-	 document.getElementById('substitue_registerProcess_list').style.display = 'block';
-	 document.getElementById('registerProcess_list').style.display = 'none';
-	 document.getElementById('extend_registerProcess_list').style.display = 'none';
-	  document.getElementById('modify_registerProcess_list').style.display = 'none';
+
+	hideOfficerFunction();
+ 	 document.getElementById('substitue_registerProcess_list').style.display = 'block';
+}
+
+function sampleProduceFunction(){
+	hideOfficerFunction();
+	document.getElementById('sampleProduceProcess_list').style.display = 'block';
+
+}
+
+function sampleImportFunction(){
+	hideFunction();
+	document.getElementById('sampleImportProcess_list').style.display = 'block';
+
 }
